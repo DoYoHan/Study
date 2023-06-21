f8717806b48c637105e641555eefef27d186f06e
DoYoHan <124124900+DoYoHan@users.noreply.github.com>
2023-06-21 17:28:43 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,12 @@
+function solution(numbers, direction) {
+    let answer = [];
+
+    if ("right" == direction) {
+        numbers.unshift(numbers.pop());
+    } else {
+        numbers.push(numbers.shift());
+    }
+
+    answer = numbers;
+    return answer;
+}
